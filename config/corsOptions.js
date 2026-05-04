acd86cca41f63d658bdbb2eb480beff17a1478ef
andrewjumperdev <andrew.jumper.ent@gmail.com>
2026-05-04 23:35:30 +0200

    fix: add www and github.io to CORS allowed origins

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "https://maisonpourpets.com",
+  "https://www.maisonpourpets.com",
+  "https://andrewjumperdev.github.io",
 ];
 
 export const corsOptions = {
